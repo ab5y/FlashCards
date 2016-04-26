dde6461ed7532da1e27dd783d4be6fb922479a00
ab5y <ab5s10@gmail.com>
2016-04-26 15:05:58 +0530

    first commit

---
+++ app/src/main/java/com/ab5y/flashcards/FlashCardActivity.java
@@ -0,0 +1,78 @@
+package com.ab5y.flashcards;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ab5y.flashcards.R;
+
+import static android.view.MotionEvent.*;
+
+
+public class FlashCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_card);
+
+        RelativeLayout rlMainFlashCard = (RelativeLayout) findViewById(R.id.rlFlashCard);
+        rlMainFlashCard.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                TextView tvCardTopic = (TextView) findViewById(R.id.tvCardTopic);
+
+                switch (event.getAction() & ACTION_MASK) {
+                    case ACTION_UP:
+                        tvCardTopic.setVisibility(View.INVISIBLE);
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                    case ACTION_DOWN:
+                        tvCardTopic.setVisibility(View.VISIBLE);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_flash_card, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//    @Override
+//    public boolean dispatchTouchEvent(MotionEvent event){
+//
+//    }
+
+    public void newCard(MenuItem menuItem) {
+        Intent intent = new Intent(this, NewCardActivity.class);
+        startActivity(intent);
+    }
+}
